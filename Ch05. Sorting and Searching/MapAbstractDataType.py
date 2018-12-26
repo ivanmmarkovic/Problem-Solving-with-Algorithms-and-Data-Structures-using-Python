db4978516c6088137cfe6d9981e44a9a69ec72c8
Ivan Markovic <ivanmdmarkovic@gmail.com>
2018-12-26 10:46:55 -0800

    Update MapAbstractDataType.py delete function

--- Ch05. Sorting and Searching/MapAbstractDataType.py
+++ Ch05. Sorting and Searching/MapAbstractDataType.py
@@ -1,5 +1,6 @@
 
 class HashTable:
+
     def __init__(self, size):
         self.keys = [None] * size
         self.values = [None] * size
@@ -7,7 +8,7 @@ class HashTable:
     def put(self, key, value):
         hash = self.hash(key, len(self.keys))
         if self.keys[hash] == None:
-            self.keys[hash] = key 
+            self.keys[hash] = key
             self.values[hash] = value
         elif self.keys[hash] == key:
             self.values[hash] = value
@@ -18,7 +19,7 @@ class HashTable:
             if self.keys[newHash] == None:
                 self.keys[newHash] = key
                 self.values[newHash] = value
-            elif self.keys[newHash] == key:
+            elif self.keys[newHash] == key :
                 self.values[newHash] = value
 
     def get(self, key):
@@ -38,6 +39,32 @@ class HashTable:
             else:
                 return None
 
+    def delete(self, key):
+        hash = self.hash(key, len(self.keys))
+        if self.keys[hash] == None:
+            return None
+        elif self.keys[hash] == key:
+            toReturn = self.values[hash]
+            self.keys[hash] = None
+            self.values[hash] = None
+            return toReturn
+        else:
+            newHash = self.rehash(hash, len(self.keys))
+            while newHash != hash and self.keys[newHash] != None and self.keys[newHash] != key:
+                newHash = self.rehash(newHash, len(self.keys))
+            if self.keys[newHash] == None:
+                return None
+            elif self.keys[newHash] == key:
+                toReturn = self.values[newHash]
+                self.keys[newHash] = None
+                self.values[newHash] = None
+                return toReturn
+            else:
+                return None
+
+    def show(self):
+        print(self.keys)
+        print(self.values)
 
     def hash(self, key, size):
         return key % size
@@ -45,11 +72,24 @@ class HashTable:
     def rehash(self, oldHash, size):
         return (oldHash + 1) % size
 
-    
+
 
 ht = HashTable(11)
-ht.put(11, "eleven")
-ht.put(22, "twenty-two")
-ht.put(33, "thirty-three")
-ht.put(12, "twelve")
-print(ht.get(11), ht.get(33), ht.get(133))
+print("put ##########################")
+ht.put(11, "11 - string")
+ht.put(22, "22 - string")
+ht.put(33, "33 - string")
+ht.put(44, "44 - string")
+ht.put(12, "12 - string")
+ht.put(10, "10 - string")
+print("get ##########################")
+print(ht.get(22), ht.get(33), ht.get(117))
+
+ht.show()
+
+print("delete ##########################")
+print(ht.delete(33), ht.delete(15))
+print(ht.get(22), ht.get(33), ht.get(117))
+
+ht.show()
+
