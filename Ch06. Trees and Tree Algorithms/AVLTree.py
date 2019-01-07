3300b1b3b9a7c2eff43a3de273650dd9017ee6a1
Ivan Markovic <ivanmdmarkovic@gmail.com>
2019-01-07 08:52:16 -0800

    AVLTree - not finished

---
+++ Ch06. Trees and Tree Algorithms/AVLTree.py
@@ -0,0 +1,53 @@
+# NOT FINISHED
+class AVLTree:
+    def __init__(self,key = None, payload = None, parent = None, 
+            leftChild = None, rightChild = None, height = 0, balance = 0):
+        self.key = key
+        self.payload = payload
+        self.parent = parent
+        self.leftChild = leftChild
+        self.rightChild = rightChild
+        self.height = height
+        self.balance = balance
+
+    def isLeftChild(self):
+        return self.parent.leftChild == self
+
+    def isRightChild(self):
+        return self.parent.rightChild == self
+
+    def put(self, key, payload):
+        if self.key == None:
+            self.payload = payload
+        elif self.key == key:
+            self.payload = payload
+        elif self.key > key:
+            if self.leftChild != None:
+                self.leftChild.put(key, payload)
+            else:
+                self.leftChild = AVLTree(key, payload, self)
+                self.leftChild.height = self.leftChild.updateHeight()
+                self.updateBalance(self.leftChild)
+        elif self.key < key:
+            if self.rightChild != None:
+                self.rightChild.put(key, payload)
+            else:
+                self.rightChild = AVLTree(key, payload, self)
+                self.rightChild.height = self.rightChild.updateHeight()
+                self.updateBalance(self.rightChild)
+
+    def updateHeigth(self):
+        if self.parent == None:
+            return 0
+        else:
+            return 1 + self.parent.updateHeigth()
+
+    def updateBalance(self, node):
+        if node.isLeftChild():
+            self.balance += 1
+        elif node.isRightChild():
+            self.balance -= 1
+        if self.balance != 0 and self.parent != None:
+                self.parent.updateBalance(self)
+
+
