35bd597a2e41c21fd7c6e6293211dc4ab9859190
Ivan Markovic <ivanmdmarkovic@gmail.com>
2019-01-24 10:46:59 -0800

    Update AVLTree.py

--- Ch06. Trees and Tree Algorithms/AVLTree.py
+++ Ch06. Trees and Tree Algorithms/AVLTree.py
@@ -5,15 +5,26 @@
 
 
 class AVLTree:
-    def __init__(self,key = None, payload = None, parent = None, 
-            leftChild = None, rightChild = None, height = 0, balance = 0):
+
+    def __init__(self, key = None, payload = None, parent = None, leftChild = None, rightChild = None, balanceFactor = 0):
         self.key = key
         self.payload = payload
         self.parent = parent
         self.leftChild = leftChild
         self.rightChild = rightChild
-        self.height = height
-        self.balance = balance
+        self.balanceFactor = balanceFactor
+
+    def hasParent(self):
+        return self.parent != None
+
+    def getParent(self):
+        return self.parent
+
+    def isRoot(self):
+        return self.parent == None
+    
+    def isLeaf(self):
+        return self.leftChild == None and self.rightChild == None
 
     def isLeftChild(self):
         return self.parent.leftChild == self
@@ -21,20 +32,33 @@ class AVLTree:
     def isRightChild(self):
         return self.parent.rightChild == self
 
+    def hasLeftChild(self):
+        return self.leftChild != None
+
+    def hasRightChild(self):
+        return self.rightChild != None
+
+    def hasAnyChildren(self):
+        return self.rightChild or self.leftChild
+
+    def hasBothChildren(self):
+        return self.hasLeftChild() and self.hasRightChild()
+
     def put(self, key, payload):
         if self.key == None:
+            self.key = key
             self.payload = payload
         elif self.key == key:
             self.payload = payload
         elif self.key > key:
-            if self.leftChild != None:
+            if self.hasLeftChild():
                 self.leftChild.put(key, payload)
             else:
                 self.leftChild = AVLTree(key, payload, self)
                 self.leftChild.height = self.leftChild.updateHeight()
                 self.updateBalance(self.leftChild)
         elif self.key < key:
-            if self.rightChild != None:
+            if self.hasRightChild():
                 self.rightChild.put(key, payload)
             else:
                 self.rightChild = AVLTree(key, payload, self)
@@ -46,66 +70,62 @@ class AVLTree:
             return 0
         else:
             return 1 + self.parent.updateHeigth()
-
+            
     def updateBalance(self, node):
         if node.isLeftChild():
-            self.balance += 1
+            self.balanceFactor += 1
         elif node.isRightChild():
-            self.balance -= 1
-
-        if self.balance < -1 or self.balance > 1:
+            self.balanceFactor -= 1
+        
+        if self.balanceFactor < -1 or self.balanceFactor > 1:
             self.rebalance()
             return
 
-        if self.balance != 0 and self.parent != None:
-                self.parent.updateBalance(self)
+        if self.hasParent() and self.balanceFactor != 0:
+            self.parent.updateBalance(self)
 
     def rebalance(self):
-        if self.balance < 0:
-            if self.rightChild.balance > 0:
+        if self.balanceFactor > 0:
+            if self.leftChild.balanceFactor < 0:
+                self.leftChild.rotateLeft()
+                self.rotateRight()
+            else:
+                self.rotateRight()
+        elif self.balanceFactor < 0:
+            if self.rightChild.balanceFactor > 0:
                 self.rightChild.rotateRight()
                 self.rotateLeft()
             else:
                 self.rotateLeft()
-        elif self.balance > 0:
-            if self.leftChild.balance < 0:
-                self.leftChild().rotateLeft()
-                self.rotateRight()
-            else:
-                self.rotateRight()
-        
 
     def rotateLeft(self):
         oldRoot = self
-        newRoot = self.rightChild
-        # child
+        newRoot = self.leftChild
+        # children
         oldRoot.rightChild = newRoot.leftChild
-        if newRoot.leftChild != None:
-            newRoot.leftChild.parent = oldRoot 
+        if newRoot.hasLeftChild():
+            newRoot.leftChild.parent = oldRoot
         # oldRoot parent
         newRoot.parent = oldRoot.parent
-        if oldRoot.parent != None:
+        if oldRoot.hasParent():
             if oldRoot.isLeftChild():
                 oldRoot.parent.leftChild = newRoot
             elif oldRoot.isRightChild():
                 oldRoot.parent.rightChild = newRoot
         # set parent child relation
-        newRoot.leftChild = oldRoot
         oldRoot.parent = newRoot
-
-        oldRoot.balance = oldRoot.balance + 1 - min(newRoot.balance, 0)
-        newRoot.balance = newRoot.balance + 1 + max(oldRoot.balance, 0) 
+        newRoot.leftChild = oldRoot
 
     def rotateRight(self):
         oldRoot = self
         newRoot = oldRoot.leftChild
-        # child
+        # children
         oldRoot.leftChild = newRoot.rightChild
         if newRoot.rightChild != None:
             newRoot.rightChild.parent = oldRoot
         # oldRoot parent
         newRoot.parent = oldRoot.parent
-        if oldRoot.parent != None:
+        if oldRoot.hasParent():
             if oldRoot.isLeftChild():
                 oldRoot.parent.leftChild = newRoot
             elif oldRoot.isRightChild():
@@ -113,6 +133,4 @@ class AVLTree:
         # set parent child relation
         newRoot.rightChild = oldRoot
         oldRoot.parent = newRoot
-
-
 
