8994cbfc24cd0f5c24c17bfccef7997d70d545d6
Ivan Markovic <ivanmdmarkovic@gmail.com>
2019-02-01 06:43:15 -0800

    Update LinkedList.py

--- Ch03. Basic Data Structures/DoubleLinkedList/LinkedList.py
+++ Ch03. Basic Data Structures/DoubleLinkedList/LinkedList.py
@@ -1,123 +1,169 @@
+
 from Node import Node
 
 class LinkedList:
     def __init__(self):
-        self.head = None
-        self.tail = None
-    
+        self.head = self.tail = None
+
     def isEmpty(self):
         return self.head == None
 
+    def numberOfElements(self):
+        count = 0
+        tmp = self.head
+        while tmp != None:
+            count += 1
+            tmp = tmp.next
+
+        return count
+
     def printAll(self):
         result = ""
-        currentNode = self.head
-        while currentNode != None:
-            result += (str(currentNode.payload) + ", ")
-            currentNode = currentNode.next
-        print(result)
-
-    def size(self):
-        counter = 0
-        currentNode = self.head
-        while currentNode != None:
-            counter += 1
-            currentNode = currentNode.next
-        return counter
+        tmp = self.head
+        while tmp != None:
+            result += str(tmp.payload) + ", "
+            tmp = tmp.next
+        return result
 
     def addToHead(self, payload):
         if self.isEmpty():
-            self.head = self.tail = Node(payload, None, None)
+            self.head = self.tail = Node(payload)
         else:
             self.head = Node(payload, None, self.head)
-            self.head.next.previous = self.head
+            self.head.next.prev = self.head
 
     def addToTail(self, payload):
         if self.isEmpty():
-            self.head = self.tail = Node(payload, None, None)
+            self.head = self.tail = Node(payload)
         else:
-            self.tail.next = Node(payload, self.tail, None)
+            self.tail.next = Node(payload, self.tail)
             self.tail = self.tail.next
 
     def removeFromHead(self):
         if self.isEmpty():
             return None
         else:
-            payloadToReturn = self.head.payload
+            returnValue = self.head.payload
             if self.head == self.tail:
                 self.head = self.tail = None
             else:
-                self.head = self.head.next
-                self.head.previous = None
-            return payloadToReturn
+                nodeToDelete = self.head
+                self.head =self.head.next
+                self.head.prev = None
+                nodeToDelete = None
+            return returnValue
 
     def removeFromTail(self):
         if self.isEmpty():
             return None
         else:
-            payloadToReturn = self.tail.payload
+            returnValue = self.tail.payload
             if self.head == self.tail:
                 self.head = self.tail = None
             else:
-                self.tail = self.tail.previous
+                nodeToDelete = self.tail
+                self.tail = self.tail.prev
                 self.tail.next = None
-            return payloadToReturn
+                nodeToDelete = None
+            return returnValue
 
-    def deleteNodeOnIndex(self, index):
-        if(index >= self.size() or index < 0 or self.isEmpty()):
-            return 
-        else:
-            counter = 0
-            prevNode = None
-            currentNode = self.head
-            while counter < index:
-                prevNode = currentNode
-                currentNode = currentNode.next
-                counter += 1
-            if prevNode == None: # delete self.head
-                if self.head == self.tail:
-                    self.tail = self.head = None
-                else:
-                    self.head = self.head.next
-                    self.head.previous = None
-            else:
-                prevNode.next = currentNode.next
-                if prevNode.next == None:
-                    self.tail = prevNode
-                else:
-                    prevNode.next.previous = prevNode
-                currentNode = None
-
-    def deleteNodesWithValue(self, payload):
+    def deleteNodesWithValue(self, value):
         if self.isEmpty():
             return
-        elif self.head != self.tail: # two or more Nodes
+        else:
             currentNode = self.head
             while currentNode.next != None:
-                if currentNode.next.payload == payload:
-                    currentNode.next = currentNode.next.next
-                    if currentNode.next != None:
-                        currentNode.next.previous = currentNode
+                if currentNode.next.payload == value:
+                    if currentNode.next.next == None:
+                        currentNode.next = None
+                    else:
+                        currentNode.next = currentNode.next.next
+                        currentNode.next.prev = currentNode
                 else:
                     currentNode = currentNode.next
             self.tail = currentNode
-        # check self.head
-        if self.head.payload == payload:
-            if self.head == self.tail:
-                self.head = self.tail = None
+        if self.head.payload == value:
+            self.removeFromHead()
+
+    def deleteOnIndex(self, index):
+        size = self.numberOfElements()
+        if self.isEmpty():
+            pass
+        elif index < 0 or index >= size:
+            pass
+        else:
+            if index == 0:
+                self.removeFromHead()
+            elif index == size - 1:
+                self.removeFromTail()
             else:
-                self.head = self.head.next
-                self.head.previous = None
+                counter = 1
+                nodeToDelete = self.head.next
+                while counter < index:
+                    nodeToDelete = nodeToDelete.next
+                    counter += 1
+
+                nodeToDelete.prev.next = nodeToDelete.next
+                nodeToDelete.next.prev = nodeToDelete.prev
+
+    def insertBefore(self, listValue, newValue):
+        tmp = self.head
+        while tmp != None:
+            if tmp.payload == listValue:
+                if tmp == self.head:
+                    self.addToHead(newValue)
+                else:
+                    newNode = Node(newValue, tmp.prev, tmp)
+                    tmp.prev.next = newNode
+                    tmp.prev = newNode
+            tmp = tmp.next
+
+    def insertAfter(self, listValue, newValue):
+        tmp = self.head
+        while tmp != None:
+            if tmp.payload == listValue:
+                if tmp == self.tail:
+                    self.addToTail(newValue)
+                else:
+                    newNode = Node(newValue, tmp, tmp.next)
+                    tmp.next.prev = newNode
+                    tmp.next = newNode
+                tmp = tmp.next
+            tmp = tmp.next
 
     def sort(self):
         outer = self.head
-        while outer != None:
+        while outer != self.tail:
             inner = self.tail
             while inner != outer:
-                if inner.previous.payload > inner.payload:
-                    tmp = inner.previous.payload
-                    inner.previous.payload = inner.payload
+                if inner.prev.payload > inner.payload:
+                    tmp = inner.prev.payload
+                    inner.prev.payload = inner.payload
                     inner.payload = tmp
-                inner = inner.previous
+                inner = inner.prev
             outer = outer.next
-        
 
+
+l = LinkedList()
+
+for i in range(1, 6):
+    l.addToHead(i)
+
+l.addToHead(5)
+l.addToHead(5)
+l.addToHead(5)
+
+for i in range(5, 10):
+    l.addToHead(i)
+
+
+l.addToTail(5)
+l.addToTail(5)
+l.addToTail(5)
+l.addToHead(5)
+l.addToHead(5)
+l.addToHead(5)
+
+print(l.printAll())
+l.deleteNodesWithValue(5)
+print(l.printAll())
