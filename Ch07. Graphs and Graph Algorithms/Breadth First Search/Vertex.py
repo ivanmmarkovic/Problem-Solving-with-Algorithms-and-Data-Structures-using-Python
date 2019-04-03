2ab5f49f18d3536e25419909a913c563043f0128
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2019-04-03 01:51:56 -0700

    graph algorithms removed

--- Ch07. Graphs and Graph Algorithms/Breadth First Search/Vertex.py
+++
@@ -1,6 +0,0 @@
-class Vertex:
-    def __init__(self, label = None, index = None):
-        self.label = label
-        self.index = index
-        self.visited = False
-        self.inserted = False
