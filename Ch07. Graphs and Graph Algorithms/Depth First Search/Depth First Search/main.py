e954a139424c0916f1a7dbbd00a16ba6c240650a
Ivan Markovic <ivanmdmarkovic@gmail.com>
2019-07-06 08:02:12 -0700

    Add files via upload

---
+++ Ch07. Graphs and Graph Algorithms/Depth First Search/Depth First Search/main.py
@@ -0,0 +1,22 @@
+from graph import Graph
+
+graph = Graph()
+
+myVertices = ['A','B','C','D','E','F','G','H','I']
+# add vertices
+for i in range(len(myVertices)):
+    graph.add_vertex(myVertices[i])
+
+graph.add_edge('A', 'B')
+graph.add_edge('A', 'C')
+graph.add_edge('A', 'D')
+graph.add_edge('C', 'D')
+graph.add_edge('C', 'G')
+graph.add_edge('D', 'G')
+graph.add_edge('D', 'H')
+graph.add_edge('B', 'E')
+graph.add_edge('B', 'F')
+graph.add_edge('E', 'I')
+
+graph.dfs("A", "H")
+print(graph.to_string())
