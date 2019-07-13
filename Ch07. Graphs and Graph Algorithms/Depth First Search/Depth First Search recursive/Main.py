b3f4b9f3d96f6530f43a7d8d9acb23ac734153e2
Ivan Markovic <ivanmdmarkovic@gmail.com>
2019-07-13 14:59:32 -0700

    Depth First Search recursive
    Depth First Search recursive

---
+++ Ch07. Graphs and Graph Algorithms/Depth First Search/Depth First Search recursive/Main.py
@@ -0,0 +1,26 @@
+from Graph import Graph
+
+graph = Graph()
+
+vertices = ["a", "b", "c", "d", "e", "f", "g"]
+for vertex in vertices:
+    graph.add_vertex(vertex)
+
+
+graph.add_edge("a", "c")
+graph.add_edge("c", "d")
+graph.add_edge("c", "f")
+graph.add_edge("a", "b")
+graph.add_edge("d", "e")
+graph.add_edge("b", "c")
+graph.add_edge("b", "g")
+
+graph.dfs("a")
+
+print(graph.print_path("e"))
+
+
+
+
+
+
