fd1363e48cf00e0ef7ceeb8a6f1d998940a03a74
Ivan Markovic <ivanmdmarkovic@gmail.com>
2019-08-06 08:56:03 -0700

    Update AVLTree.py

--- Ch06. Trees and Tree Algorithms/AVLTree.py
+++ Ch06. Trees and Tree Algorithms/AVLTree.py
@@ -1,8 +1,9 @@
+
+
 class AVLTree:
-    def __init__(self, key=None, payload=None, parent=None, leftChild=None, rightChild=None,
-                 leftSubtreeHeight=0, rightSubtreeHeight=0, balanceFactor=0):
+    def __init__(self, key: int = None, parent = None, leftChild = None, rightChild = None,
+        leftSubtreeHeight: int = 0, rightSubtreeHeight: int = 0, balanceFactor: int = 0):
         self.key = key
-        self.payload = payload
         self.parent = parent
         self.leftChild = leftChild
         self.rightChild = rightChild
@@ -10,13 +11,22 @@ class AVLTree:
         self.rightSubtreeHeight = rightSubtreeHeight
         self.balanceFactor = balanceFactor
 
-    def hasParent(self):
+    def isRoot(self)-> bool:
+        return self.parent is None
+
+    def isLeftChild(self)-> bool:
+        return self.parent.leftChild == self
+
+    def isRightChild(self)-> bool:
+        return self.parent.rightChild == self
+
+    def hasParent(self)-> bool:
         return self.parent is not None
 
-    def hasLeftChild(self):
+    def hasLeftChild(self)-> bool:
         return self.leftChild is not None
 
-    def hasRightChild(self):
+    def hasRightChild(self)-> bool:
         return self.rightChild is not None
 
     def isLeaf(self):
@@ -25,33 +35,24 @@ class AVLTree:
     def hasBothChildren(self):
         return self.hasLeftChild() and self.hasRightChild()
 
-    def isRoot(self):
-        return self.parent is None
-
-    def isLeftChild(self):
-        return self.parent.leftChild == self
-
-    def isRightChild(self):
-        return self.parent.rightChild == self
-
-    def findMax(self):
-        if self.key is None:
-            return None
-        elif self.hasRightChild():
-            return self.rightChild.findMax()
+    def put(self, key: int):
+        if self.key is None or self.key == key:
+            self.key = key
+        elif key < self.key:
+            if self.hasLeftChild():
+                self.leftChild.put(key)
+            else:
+                self.leftChild = BinarySearchTree(key, self)
+                self.updateHeight(self.leftChild)
         else:
-            return self
-
-    def findMin(self):
-        if self.key is None:
-            return None
-        elif self.hasLeftChild():
-            return self.leftChild.findMin()
-        else:
-            return self
-
+            if self.hasRightChild():
+                self.rightChild.put(key)
+            else:
+                self.rightChild = BinarySearchTree(key, self)
+                self.updateHeight(self.rightChild)
+    
     def get(self, key):
-        if self.key == None:
+        if self.key is None:
             return None
         elif self.key == key:
             return self
@@ -59,42 +60,142 @@ class AVLTree:
             if self.hasLeftChild():
                 return self.leftChild.get(key)
             else:
-                return None
-        elif self.key < key:
+                return self
+        else:
             if self.hasRightChild():
                 return self.rightChild.get(key)
             else:
                 return None
 
-    def put(self, key, payload=None, parent=None):
-        if self.key is None or self.key == key:
-            self.payload = payload
+    def findMax(self):
+        if self.key is None:
+            return None
+        else:
+            if self.hasRightChild():
+                return self.rightChild.findMax()
+            else:
+                return self
+
+    def findMin(self):
+        if self.key is None:
+            return None
+        else:
+            if self.hasLeftChild():
+                return self.leftChild.findMin()
+            else:
+                return self
+
+    def contains(self, key)-> bool:
+        if self.key is None:
+            return False
+        elif self.key == key:
+            return True
         elif self.key > key:
             if self.hasLeftChild():
-                self.leftChild.put(key, payload)
+                return self.leftChild.contains(key)
             else:
-                self.leftChild = AVLTree(key, payload, self)
-                self.updateBalanceAfterPut(self.leftChild)
-        elif self.key < key:
+                return False
+        else:
             if self.hasRightChild():
-                self.rightChild.put(key, payload)
+                return self.rightChild.contains(key)
             else:
-                self.rightChild = AVLTree(key, payload, self)
-                self.updateBalanceAfterPut(self.rightChild)
+                return False
 
-    def updateBalanceAfterPut(self, avl):
-        if avl.isLeftChild():
+    def size(self):
+        count: int = 1
+        if self.hasLeftChild():
+            count += self.leftChild.size()
+        if self.hasRightChild():
+            count += self.rightChild.size()
+        return count
+
+    def delete(self, key):
+        tmp = self.get(key)
+        if tmp is None:
+            return
+        parent = None
+        if tmp.isLeaf():
+            if tmp.isRoot():
+                tmp.key = None
+            else:
+                parent = tmp.parent
+                if tmp.isLeftChild():
+                    tmp.parent.leftChild = None
+                else:
+                    tmp.parent.rightChild = None
+        elif tmp.hasBothChildren():
+            maxBinarySearchTree = tmp.leftChild.findMax()
+            tmpKey: int = maxBinarySearchTree.key
+            self.delete(tmpKey)
+            tmp.key = tmpKey
+        elif tmp.hasLeftChild():
+            parent = tmp.parent
+            if tmp.isRoot():
+                tmp.leftChild.parent = None
+            elif tmp.isRightChild():
+                tmp.parent.rightChild = tmp.leftChild
+                tmp.leftChild.parent = tmp.parent
+            else:
+                tmp.parent.leftChild = tmp.leftChild
+                tmp.leftChild = tmp.parent
+        else:
+            parent = tmp.parent
+            if tmp.isRoot():
+                tmp.rightChild.parent = None
+            elif tmp.isRightChild():
+                tmp.parent.rightChild = tmp.rightChild
+                tmp.rightChild.parent = tmp.parent
+            else:
+                tmp.parent.leftChild = tmp.rightChild
+                tmp.rightChild.parent = tmp.parent
+        if parent is not None:
+            parent.updateHeightAfterDeletion()
+
+    def inorder(self):
+        if self.hasLeftChild():
+            self.leftChild.inorder()
+        print(self.key)
+        if self.hasRightChild():
+            self.rightChild.inorder()
+
+    def updateHeight(self, tree):
+        if tree.isLeftChild():
             self.leftSubtreeHeight += 1
-        if avl.isRightChild():
+        if tree.isRightChild():
             self.rightSubtreeHeight += 1
         self.balanceFactor = self.leftSubtreeHeight - self.rightSubtreeHeight
-
-        if self.balanceFactor > 1 or self.balanceFactor < -1:
+        if self.balanceFactor < -1 or self.balanceFactor > 1:
             self.rebalance()
             return
-
         if self.balanceFactor != 0 and self.hasParent():
-            self.parent.updateBalanceAfterPut(self)
+            self.parent.updateHeight(self)
+
+    def updateHeightAfterRotation(self):
+        if self.hasLeftChild():
+            self.leftSubtreeHeight = max(self.leftChild.leftSubtreeHeight, self.leftChild.rightSubtreeHeight) + 1
+        else:
+            self.leftSubtreeHeight = 0
+        if self.hasRightChild():
+            self.rightSubtreeHeight = max(self.rightChild.leftSubtreeHeight, self.rightChild.rightSubtreeHeight) + 1
+        else:
+            self.rightSubtreeHeight = 0
+        self.balanceFactor = self.leftSubtreeHeight - self.rightSubtreeHeight
+
+    def updateHeightAfterDeletion(self):
+        if self.hasLeftChild():
+            self.leftSubtreeHeight = max(self.leftChild.leftSubtreeHeight, self.leftChild.rightSubtreeHeight) + 1
+        else:
+            self.leftSubtreeHeight = 0
+        if self.hasRightChild():
+            self.rightSubtreeHeight = max(self.rightChild.leftSubtreeHeight, self.rightChild.rightSubtreeHeight) + 1
+        else:
+            self.rightSubtreeHeight = 0
+        self.balanceFactor = self.leftSubtreeHeight - self.rightSubtreeHeight
+        if self.balanceFactor < -1 or self.balanceFactor > 1:
+            self.rebalance()
+            return
+        if self.balanceFactor != 0 and self.hasParent():
+            self.parent.updateHeight(self)
 
     def rebalance(self):
         if self.balanceFactor > 0:
@@ -105,7 +206,7 @@ class AVLTree:
                 self.rotateRight()
         elif self.balanceFactor < 0:
             if self.rightChild.balanceFactor > 0:
-                self.leftChild.rotateRihgt()
+                self.rightChild.rotateRight()
                 self.rotateLeft()
             else:
                 self.rotateLeft()
@@ -122,8 +223,8 @@ class AVLTree:
                 oldRoot.parent.leftChild = newRoot
             else:
                 oldRoot.parent.rightChild = newRoot
-        oldRoot.parent = newRoot
         newRoot.leftChild = oldRoot
+        oldRoot.parent = newRoot
 
         oldRoot.updateHeightAfterRotation()
         newRoot.updateHeightAfterRotation()
@@ -146,104 +247,5 @@ class AVLTree:
         oldRoot.updateHeightAfterRotation()
         newRoot.updateHeightAfterRotation()
 
-    def updateHeightAfterRotation(self):
-        if self.hasLeftChild():
-            self.leftSubtreeHeight = max(self.leftChild.leftSubtreeHeight, self.leftChild.rightSubtreeHeight) + 1
-        else:
-            self.leftSubtreeHeight = 0
-        if self.hasRightChild():
-            self.rightSubtreeHeight = max(self.rightChild.leftSubtreeHeight, self.rightChild.rightSubtreeHeight) + 1
-        else:
-            self.rightSubtreeHeight = 0
-        self.balanceFactor = self.leftSubtreeHeight - self.rightSubtreeHeight
 
-    def delete(self, key):
-        nodeToDelete = self.get(key)
-        if nodeToDelete.isLeaf():
-            if nodeToDelete.hasParent():
-                parent = nodeToDelete.parent
-                if nodeToDelete.isLeftChild():
-                    nodeToDelete.parent.leftChild = None
-                    parent.updateAfterDeletion()
-                else:
-                    nodeToDelete.parent.rightChild = None
-                    parent.updateAfterDeletion()
-            else:
-                nodeToDelete = None
-        elif nodeToDelete.hasBothChildren():
-            maxInLeftBranch = nodeToDelete.leftChild.findMax()
-            tmpKey = maxInLeftBranch.key
-            tmpPayload = maxInLeftBranch.payload
-            nodeToDelete.key = tmpKey
-            nodeToDelete.payload = tmpPayload
-            nodeToDelete.delete(tmpKey)
-        elif nodeToDelete.hasLeftChild():
-            if nodeToDelete.hasParent():
-                parent = nodeToDelete.parent
-                if nodeToDelete.isLeftChild():
-                    nodeToDelete.parent.leftChild = nodeToDelete.leftChild
-                    nodeToDelete.leftChild.parent = nodeToDelete.parent
-                    parent.updateAfterDeletion()
-                else:
-                    nodeToDelete.parent.rightChild = nodeToDelete.leftChild
-                    nodeToDelete.leftChild.parent = nodeToDelete.parent
-                    parent.updateAfterDeletion()
-            else:
-                nodeToDelete.leftChild.parent = None
-        elif nodeToDelete.hasRightChild():
-            if nodeToDelete.hasParent():
-                parent = nodeToDelete.parent
-                if nodeToDelete.isLeftChild():
-                    nodeToDelete.rightChild.parent = nodeToDelete.parent
-                    nodeToDelete.parent.leftChild = nodeToDelete.rightChild
-                    parent.updateAfterDeletion()
-                else:
-                    nodeToDelete.rightChild.parent = nodeToDelete.parent
-                    nodeToDelete.parent.rightChild = nodeToDelete.rightChild
-                    parent.updateAfterDeletion()
-            else:
-                nodeToDelete.rightChild.parent = None
 
-    def updateAfterDeletion(self):
-        oldBalanceFactor = self.balanceFactor
-        if self.hasLeftChild():
-            self.leftSubtreeHeight = max(self.leftChild.leftSubtreeHeight, self.leftChild.rightSubtreeHeight) + 1
-        else:
-            self.leftSubtreeHeight = 0
-        if self.hasRightChild():
-            self.rightSubtreeHeight = max(self.rightChild.leftSubtreeHeight, self.rightChild.rightSubtreeHeight) + 1
-        else:
-            self.rightSubtreeHeight = 0
-        self.balanceFactor = self.leftSubtreeHeight - self.rightSubtreeHeight
-
-        if self.balanceFactor > 1 or self.balanceFactor < -1:
-            self.rebalance()
-            return
-
-        if self.balanceFactor != oldBalanceFactor and self.hasParent():
-            self.parent.updateBalanceAfterPut()
-
-    def contains(self, key):
-        if self.key is None:
-            return False
-        elif self.key == key:
-            return True
-        elif self.key < key:
-            if self.hasRightChild():
-                return self.rightChild.contains(key)
-            else:
-                return False
-        elif self.key > key:
-            if self.hasLeftChild():
-                return self.leftChild.contains(key)
-            else:
-                return False
-
-    def size(self):
-        count = 1
-        if self.hasLeftChild():
-            count += self.leftChild.size()
-        if self.hasRightChild():
-            count += self.rightChild.size()
-
-        return count
