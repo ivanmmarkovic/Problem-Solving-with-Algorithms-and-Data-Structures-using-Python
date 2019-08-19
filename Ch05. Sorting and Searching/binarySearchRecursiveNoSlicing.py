942b4e0aa8c2d04b49365e49261f4c3421e55e89
Ivan Markovic <ivanmdmarkovic@gmail.com>
2019-08-19 06:48:49 -0700

    Update binarySearchRecursiveNoSlicing
    Update binarySearchRecursiveNoSlicing

--- Ch05. Sorting and Searching/binarySearchRecursiveNoSlicing.py
+++ Ch05. Sorting and Searching/binarySearchRecursiveNoSlicing.py
@@ -1,18 +1,22 @@
 
-def binarySearch(alist, item, start, end):
+def binarySearchRecursiveNoSlicing(alist: list, item: int, start: int, end: int)-> bool:
     if start > end:
         return False
     else:
-        midpoint = (start + end) // 2
+        midpoint: int = (start + end) // 2
         if alist[midpoint] == item:
             return True
         else:
             if alist[midpoint] > item:
                 return binarySearch(alist, item, start, midpoint - 1)
             else:
-                return binarySearch(alist, item, midpoint+ 1, end)
+                return binarySearch(alist, item, midpoint + 1, end)
 
 
-testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
-print(binarySearch(testlist, 3, 0, len(testlist)))
-print(binarySearch(testlist, 13, 0, len(testlist)))
+testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42, 54]
+print(binarySearchRecursiveNoSlicing(testlist, 3, 0, len(testlist) - 1))
+print(binarySearchRecursiveNoSlicing(testlist, 13, 0, len(testlist) - 1))
+
+print(binarySearchRecursiveNoSlicing(testlist, 54, 0, len(testlist) - 1))
+print(binarySearchRecursiveNoSlicing(testlist, 0, 0, len(testlist) - 1))
+
