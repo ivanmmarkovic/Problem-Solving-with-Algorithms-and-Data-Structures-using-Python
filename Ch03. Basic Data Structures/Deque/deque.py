f0145640a43f094d0c4180cb57554be923e6cc42
Ivan Markovic <ivanmdmarkovic@gmail.com>
2020-01-24 06:42:39 -0800

    Update deque.py

--- Ch03. Basic Data Structures/Deque/deque.py
+++ Ch03. Basic Data Structures/Deque/deque.py
@@ -1,14 +1,13 @@
 
 class Deque:
-
     def __init__(self):
         self.deque: list = []
 
+    def isEmpty(self)->bool:
+        return len(self.deque) == 0
+
     def size(self)->int:
         return len(self.deque)
-
-    def isEmpty(self):
-        return len(self.deque) == 0
 
     def addFront(self, item):
         self.deque.insert(0, item)
@@ -17,9 +16,12 @@ class Deque:
         self.deque.append(item)
 
     def removeFront(self):
+        if self.isEmpty():
+            raise Exception('Deque is empty')
         return self.deque.pop(0)
 
     def removeRear(self):
+        if self.isEmpty():
+            raise Exception('Deque is empty')
         return self.deque.pop()
-
 
