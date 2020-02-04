59b99b2ed439c5e2a97a364d1e743e36f0bf1ee3
Ivan Markovic <ivanmdmarkovic@gmail.com>
2020-02-04 05:22:38 -0800

    Update HashMapChaining.py

--- Ch05. Sorting and Searching/hashing/HashMapChaining.py
+++ Ch05. Sorting and Searching/hashing/HashMapChaining.py
@@ -1,85 +1,87 @@
+class KeyValue:
+    def __init__(self, key: None, value: None):
+        self.key = key
+        self.value = value
 
 class HashMap:
-    def __init__(self, size: int = 11):
-        self.size = size
-        self.keys: list = [None] * self.size
-        self.values: list = [None] * self.size
+    def __init__(self, size:int = 11):
+        self.size: int = size
+        self.items: list = [None] * self.size
         self.length: int = 0
 
     def put(self, key, value):
+        keyValue: KeyValue = KeyValue(key, value)
         hash: int = self.hash(key)
-        if self.keys[hash] is None:
-            self.keys[hash] = [key]
-            self.values[hash] = [value]
+        if self.items[hash] is None:
+            self.items[hash] = [keyValue]
             self.length += 1
         else:
-            found: bool = False
             count: int = 0
-            while count < len(self.keys[hash]) and not found:
-                if self.keys[hash][count] == key:
+            found: bool = False
+            while count < len(self.items[hash]) and not found:
+                if self.items[hash][count].key == keyValue.key:
                     found = True
                 else:
                     count += 1
             if found:
-                self.values[hash][count] = value
+                self.items[hash][count] = keyValue
             else:
-                self.keys[hash].append(key)
-                self.values[hash].append(value)
+                self.items[hash].append(keyValue)
                 self.length += 1
-            
+
     def get(self, key):
         hash: int = self.hash(key)
-        if self.keys[hash] is None:
+        if self.items[hash] is None:
             return None
         else:
-            found: bool = False
             count: int = 0
-            while count < len(self.keys[hash]) and not found:
-                if self.keys[hash][count] == key:
+            found: bool = False
+            while count < len(self.items[hash]) and not found:
+                if self.items[hash][count].key == key:
                     found = True
                 else:
                     count += 1
             if found:
-                return self.values[hash][count]
+                return self.items[hash][count].value
             else:
                 return None
 
-    def delete(self, key):
-        hash: int = self.hash(key)
-        if self.keys[hash] is None:
-            return
-        else:
-            found: bool = False
-            count: int = 0
-            while count < len(self.keys[hash]) and not found:
-                if self.keys[hash][count] == key:
-                    found = True
-                else:
-                    count += 1
-            if found:
-                self.keys[hash].pop(count)
-                self.values[hash].pop(count)
-                self.length -= 1
-            else:
-                return None
 
     def contains(self, key)->bool:
         hash: int = self.hash(key)
-        if self.keys[hash] is None:
+        if self.items[hash] is None:
             return False
         else:
-            found: bool = False
             count: int = 0
-            while count < len(self.keys[hash]) and not found:
-                if self.keys[hash][count] == key:
+            found: bool = False
+            while count < len(self.items[hash]) and not found:
+                if self.items[hash][count].key == key:
                     found = True
                 else:
                     count += 1
             return found
 
+    def delete(self, key):
+        hash: int = self.hash(key)
+        if self.items[hash] is None:
+            return None
+        else:
+            count: int = 0
+            found: bool = False
+            while count < len(self.items[hash]) and not found:
+                if self.items[hash][count].key == key:
+                    found = True
+                else:
+                    count += 1
+            if found:
+                return self.items[hash].pop(count)
+            else:
+                return None
+
     def hash(self, key):
         return key % self.size
-    
+
+
 ht: HashMap = HashMap()
 
 ht.put(11, "string 11")
@@ -90,11 +92,7 @@ ht.put(12, "string 12")
 
 ht.put(21, "string 21")
 
-print(ht.keys)
-print(ht.values)
-
 print(ht.contains(11), ht.contains(33), ht.contains(21), ht.contains(117))
-print(ht.delete(11))
+print(ht.delete(11).value)
 print(ht.contains(22))
 print(ht.get(22))
-
