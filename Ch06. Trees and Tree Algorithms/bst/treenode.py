f3a67af97cdd7ee30545b0cdc71f14315be9b72c
Ivan Markovic <ivanmdmarkovic@gmail.com>
2020-02-17 09:51:50 -0800

    Update treenode.py

--- Ch06. Trees and Tree Algorithms/bst/treenode.py
+++ Ch06. Trees and Tree Algorithms/bst/treenode.py
@@ -1,4 +1,5 @@
 
+
 class TreeNode:
     def __init__(self, key = None, parent = None, leftChild = None, rightChild = None):
         self.key = key
@@ -6,11 +7,17 @@ class TreeNode:
         self.leftChild = leftChild
         self.rightChild = rightChild
 
-    def isRoot(self)->bool:
-        return self.parent is None
+    def hasLeftChild(self)->bool:
+        return self.leftChild is not None
 
-    def hasParent(self)->bool:
-        return self.parent is not None
+    def hasRightChild(self)->bool:
+        return self.rightChild is not None
+
+    def hasBothChildren(self)->bool:
+        return self.hasLeftChild() and self.hasRightChild()
+
+    def isLeaf(self)->bool:
+        return not self.hasLeftChild() and not self.hasRightChild()
 
     def isLeftChild(self)->bool:
         return self.parent.leftChild == self
@@ -18,34 +25,14 @@ class TreeNode:
     def isRightChild(self)->bool:
         return self.parent.rightChild == self
 
-    def hasLeftChild(self)->bool:
-        return self.leftChild is not None
-
-    def hasRightChild(self)->bool:
-        return self.rightChild is not None
-
-    def hasBothChildren(self):
-        return self.hasLeftChild() and self.hasRightChild()
-
-    def isLeaf(self):
-        return not self.hasLeftChild() and not self.hasRightChild()
-
     def findMin(self):
-        if self.hasLeftChild():
-            return self.leftChild.findMin()
-        else:
-            return self
-        
-    def findMax(self):
-        if self.hasRightChild():
-            return self.rightChild.findMax()
-        else:
-            return self
+        node = self
+        while node.leftChild is not None:
+            node = node.leftChild
+        return node
 
-    def inorder(self):
-        if self is not None:
-            if self.leftChild is not None:
-                self.leftChild.inorder()
-            print(self.key, end=", ")
-            if self.rightChild is not None:
-                self.rightChild.inorder()
+    def findMax(self):
+        node = self
+        while node.rightChild is not None:
+            node = node.rightChild
+        return node
