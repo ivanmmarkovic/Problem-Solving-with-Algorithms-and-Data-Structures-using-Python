3d2afe0c902f505854fbaf3222be0cd7c075c76d
Ivan Markovic <ivanmdmarkovic@gmail.com>
2020-03-09 06:01:23 -0700

    Add files via upload

---
+++ Ch04. Recursion/fibonacci-sum.py
@@ -0,0 +1,12 @@
+﻿def fibonacci(n: int, prev: int, curr: int, acc: int):
+    if n == 1:
+        return prev
+    elif n == 2:
+        return acc + curr
+    else:
+        return fibonacci(n - 1, curr, prev + curr, acc + curr)
+
+
+print(fibonacci(5, 0, 1, 0)) # 7
+print(fibonacci(2, 0, 1, 0)) # 1
+print(fibonacci(1, 0, 1, 0)) # 0
