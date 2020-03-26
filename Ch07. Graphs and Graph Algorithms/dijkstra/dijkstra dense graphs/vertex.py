7f0a1e3fb338ed637ebba1ddc0340fd65decb030
Ivan Markovic <ivanmdmarkovic@gmail.com>
2020-03-26 07:00:56 -0700

    Update vertex.py

--- Ch07. Graphs and Graph Algorithms/dijkstra/dijkstra dense graphs/vertex.py
+++ Ch07. Graphs and Graph Algorithms/dijkstra/dijkstra dense graphs/vertex.py
@@ -1,7 +1,6 @@
 
 class Vertex:
-    def __init__(self, label: str = None, weight: int = None, index: int = None, key: int = None):
+    def __init__(self, label: str = None, weight: int = None, index: int = None):
         self.label = label
         self.weight = weight
         self.index = index
-        self.key = key
