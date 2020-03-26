11c98611a912b19ff49466742c9e168a1dfea9f0
Ivan Markovic <ivanmdmarkovic@gmail.com>
2020-03-26 06:59:33 -0700

    Update graph.py

--- Ch07. Graphs and Graph Algorithms/dijkstra/dijkstra dense graphs/graph.py
+++ Ch07. Graphs and Graph Algorithms/dijkstra/dijkstra dense graphs/graph.py
@@ -3,52 +3,52 @@ from vertex import Vertex
 class Graph:
     def __init__(self, size: int = 10):
         self.vertices: list = []
-        self.adjacencyMatrix = [[float("inf") for x in range(size)] for y in range(size)] 
-        self.adjacencyList: dict = {}
         self.prev: dict = {}
+        self.adjacencyMatrix: list = [[None for x in range(size)] for y in range(size)] 
         self.visited: dict = {}
 
-    def addVertex(self, label: str, weight:int = float("inf")):
-        vertex: Vertex = Vertex(label, weight)
-        vertex.index = len(self.vertices)
+    def addVertex(self, label: str):
+        vertex: Vertex = Vertex(label, float("inf"), len(self.vertices))
         self.vertices.append(vertex)
         self.prev[label] = None
-        self.adjacencyList[label] = []
         self.visited[label] = False
 
     def addEdge(self, label1: str, label2: str, weight: int):
         index1: int = self.findIndexByLabel(label1)
         index2: int = self.findIndexByLabel(label2)
         self.adjacencyMatrix[index1][index2] = weight
-        
+
     def dijkstra(self, label: str):
         index: int = self.findIndexByLabel(label)
-        current: Vertex = self.vertices[index]
-        current.weight = 0
-        while current is not None:
-            for i in range(len(self.adjacencyMatrix[current.index])):
-                if self.adjacencyMatrix[current.index][i] != float("inf"):
-                    if current.weight + self.adjacencyMatrix[current.index][i] < self.vertices[i].weight:
-                        self.vertices[i].weight = current.weight + self.adjacencyMatrix[current.index][i]
-                        self.prev[self.vertices[i].label] = current.label
-            self.visited[current.label] = True
-            current: Vertex = self.findCheapestVertex()
-            
-    def findCheapestVertex(self)->Vertex:
-        current: Vertex = None
-        for vertex in self.vertices:
-            if self.visited[vertex.label] == False:
-                if current is None:
-                    current = vertex
-                elif current.weight > vertex.weight:
-                    current = vertex
-        return current
-            
-    def showPath(self, label: str)->str:
+        self.vertices[index].weight = 0
+        tmpVertex: Vertex = self.vertices[index]
+        neighbour: Vertex
+        while tmpVertex is not None:            
+            for neighbourIndex in range(len(self.adjacencyMatrix[tmpVertex.index])):
+                if self.adjacencyMatrix[tmpVertex.index][neighbourIndex] is not None:
+                    neighbour = self.vertices[neighbourIndex]
+                    if tmpVertex.weight + self.adjacencyMatrix[tmpVertex.index][neighbour.index] < neighbour.weight:
+                        self.prev[neighbour.label] = tmpVertex.label
+                        neighbour.weight = tmpVertex.weight + self.adjacencyMatrix[tmpVertex.index][neighbour.index]
+            self.visited[tmpVertex.label] = True
+            tmpVertex = self.findUnvisitedVertexWithMinWeight()
+        
+    def returnPath(self, label: str)->str:
         if self.prev[label] is None:
             return label
         else:
-            return self.showPath(self.prev[label]) + " -> " + label
+            return self.returnPath(self.prev[label]) + " -> " + label
+
+    def findUnvisitedVertexWithMinWeight(self)->Vertex:
+        unvisitedVertex: Vertex = None
+        for vertex in self.vertices:
+            if not self.visited[vertex.label]:
+                if unvisitedVertex is None:
+                    unvisitedVertex = vertex
+                elif unvisitedVertex.weight > vertex.weight:
+                    unvisitedVertex = vertex
+        return unvisitedVertex
+
 
     def findIndexByLabel(self, label: str)->int:
         found: bool = False
@@ -61,5 +61,8 @@ class Graph:
         if found:
             return count
         else:
-            return None
-        
+            raise Exception("Can't find vertex with label " + label)
+
+
+
+
