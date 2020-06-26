851907fa5c5cce1aa2523e242d0d0e023f712294
Ivan Markovic <ivanmdmarkovic@gmail.com>
2020-06-26 09:17:43 -0700

    Update sum-number-binary-recursion.py

--- Ch04. Recursion/sum-number-binary-recursion.py
+++ Ch04. Recursion/sum-number-binary-recursion.py
@@ -5,16 +5,9 @@ arr: list = [1, 2, 3]
 def sum(arr: list, start: int, end: int)->int:
     if start > end:
         return 0
-    if start == end:
-        return arr[start]
     else:
-        midpoint: int = int(start + (end - start) / 2)
-        return sum(alist, start, midpoint - 1) + alist[midpoint] + sum(alist, midpoint + 1, end)
-    
-        # this works return sum(arr, start, midpoint) + sum(arr, midpoint + 1, end)
-        
-        # this doesn't return sum(arr, start, midpoint - 1) + sum(arr, midpoint, end)
-        # RecursionError: maximum recursion depth exceeded in comparison
+        midpoint: int = start + (end - start) // 2
+        return sum(arr, start, midpoint - 1) + arr[midpoint] + sum(arr, midpoint + 1, end)
 
 print(sum(arr, 0, len(arr) - 1))
 
