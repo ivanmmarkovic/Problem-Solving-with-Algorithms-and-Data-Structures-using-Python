d7d016801bfee79a59ec58124ecefc26ca165517
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2020-08-24 18:02:19 -0700

    Dijkstra priority queue impl - maps

--- Ch07. Graphs and Graph Algorithms/dijkstra/dijkstra priority queue/graph.py
+++ Ch07. Graphs and Graph Algorithms/dijkstra/dijkstra priority queue/graph.py
@@ -1,55 +1,43 @@
-from vertex import Vertex
 from priorityqueue import PriorityQueue
+from vertex import Vertex
 
 class Graph:
     def __init__(self):
-        self.vertices: list = []
-        self.adjacencyList: dict = {}
-        self.prev: dict = {}
+        self.__vertices__: dict = {}
+        self.__prev__: dict = {}
+        self.__distance__: dict = {}
+        self.__adjacency_map__: dict = {}
 
-    def addVertex(self, label: str, weight:int = float("inf")):
-        vertex: Vertex = Vertex(label, weight)
-        vertex.index = len(self.vertices)
-        self.vertices.append(vertex)
-        self.prev[label] = None
-        self.adjacencyList[label] = []
+    def add_vertex(self, label: str):
+        self.__vertices__[label] = Vertex(label)
+        self.__prev__[label] = None
+        self.__distance__[label] = 0
+        self.__adjacency_map__[label]: dict = {}
 
-    def addEdge(self, label1: str, label2: str, weight: int):
-        vertex: Vertex = Vertex(label2, weight)
-        index: int = self.findIndexByLabel(label2)
-        vertex.index = index
-        self.adjacencyList[label1].append(vertex)
+    def add_edge(self, label1: str, label2: str, weight: int = float("inf")):
+        self.__adjacency_map__[label1][label2] = Vertex(label2, weight)
 
     def dijkstra(self, label: str):
-        index: int = self.findIndexByLabel(label)
-        self.vertices[index].weight = 0
-        pq = PriorityQueue()
-        pq.buildHeap(self.vertices)
-        current: Vertex
-        while not pq.isEmpty():
-            current = pq.deleteMin()
-            for neighbour in self.adjacencyList[current.label]:
-                if current.weight + neighbour.weight < self.vertices[neighbour.index].weight:
-                    self.prev[self.vertices[neighbour.index].label] = current.label
-                    self.vertices[neighbour.index].weight = current.weight + neighbour.weight
-                    pq.decreaseKey(self.vertices[neighbour.index].key)
-            
-    def showPath(self, label: str)->str:
-        if self.prev[label] is None:
-            return label
-        else:
-            return self.showPath(self.prev[label]) + " -> " + label
-
-    def findIndexByLabel(self, label: str)->int:
-        found: bool = False
-        count: int = 0
-        while count < len(self.vertices) and not found:
-            if self.vertices[count].label == label:
-                found = True
-            else:
-                count += 1
-        if found:
-            return count
-        else:
-            return None
+        self.__vertices__[label].weight = 0
+        pq: PriorityQueue = PriorityQueue()
+        for label in self.__vertices__:
+            pq.insert(self.__vertices__[label])
         
+        while not pq.is_empty():
+            v: Vertex = pq.delete_min()
+            for neighbour_label in self.__adjacency_map__[v.label]:
+                neighbour: Vertex = self.__adjacency_map__[v.label][neighbour_label]
+                vertex: Vertex = self.__vertices__[neighbour_label]
+                if v.weight + neighbour.weight < vertex.weight:
+                    self.__prev__[neighbour_label] = v.label
+                    self.__distance__[neighbour_label] = self.__distance__[v.label] + 1
+                    vertex.weight = v.weight + neighbour.weight
+                    pq.decrease_key(vertex.key)
+    
+    def show_path(self, end: str) -> str:
+        if self.__prev__[end] is None:
+            return end
+        else:
+            return self.show_path(self.__prev__[end]) + " -> " + end
+
+    
--- Ch07. Graphs and Graph Algorithms/dijkstra/dijkstra priority queue/priorityqueue.py
+++ Ch07. Graphs and Graph Algorithms/dijkstra/dijkstra priority queue/priorityqueue.py
@@ -1,60 +1,63 @@
 from vertex import Vertex
 
 class PriorityQueue:
-    def __init__(self, size: int = 0):
-        self.heap: list = [None]
-        self.size = size
+    def __init__(self):
+        self.__queue__: list = [None]
+        self.__pointer__: int = 0
 
-    def isEmpty(self)->bool:
-        return self.size == 0
+    def is_empty(self) -> bool:
+        return self.__pointer__ == 0
 
     def insert(self, vertex: Vertex):
-        self.size += 1
-        vertex.key = self.size
-        self.heap.append(vertex)
-        self.percUp(self.size)
+        self.__pointer__ += 1
+        self.__queue__.append(vertex)
+        vertex.key = self.__pointer__
+        self.__perc_up__(self.__pointer__)
 
-    def percUp(self, index: int):
-        while index // 2 > 0:
-            if self.heap[index].weight < self.heap[index//2].weight:
-                self.heap[index], self.heap[index//2] = self.heap[index//2], self.heap[index]
-                self.heap[index].key = index
-                self.heap[index//2].key = index // 2
-            index = index // 2
+    def __perc_up__(self, pointer: int):
+        while pointer // 2 > 0:
+            if self.__queue__[pointer].weight < self.__queue__[pointer // 2].weight:
+                self.__queue__[pointer], self.__queue__[pointer // 2] = self.__queue__[pointer // 2], self.__queue__[pointer]
+                self.__queue__[pointer].key = pointer
+                self.__queue__[pointer // 2].key = pointer // 2
+            pointer = pointer // 2
 
-    def decreaseKey(self, index):
-        self.percUp(index)
+    def decrease_key(self, key: int):
+        self.__perc_up__(key)
 
-    def deleteMin(self)->Vertex:
-        if self.isEmpty():
-            return None
-        else:
-            tmp: Vertex = self.heap[1]
-            self.heap[1] = self.heap[self.size]
-            self.heap[1].key = 1
-            self.heap.pop()
-            self.size -= 1
-            self.percDown(1)
-            return tmp
+    def get_min(self) -> Vertex:
+        if self.is_empty():
+            raise Exception("Priority Queue is empty")
+        return self.__queue__[1]
 
-    def percDown(self, index: int):
-        while index * 2 <= self.size:
-            minIndex: int = self.getMinIndex(index)
-            if self.heap[index].weight > self.heap[minIndex].weight:
-                self.heap[index], self.heap[minIndex] = self.heap[minIndex], self.heap[index]
-                self.heap[index].key = index
-                self.heap[minIndex].key = minIndex 
-            index = minIndex
+    def delete_min(self) -> Vertex:
+        if self.is_empty():
+            raise Exception("Priority Queue is empty")
+        min_vertex: Vertex = self.__queue__[1]
+        self.__queue__[1] = self.__queue__[self.__pointer__]
+        self.__queue__[1].key = 1
+        self.__pointer__ -= 1
+        self.__queue__.pop()
+        self.__perc_down__(1)
+        return min_vertex
 
-    def getMinIndex(self, index: int):
-        if index * 2 + 1 > self.size:
+    def __perc_down__(self, index: int):
+        while index * 2 <= self.__pointer__:
+            min_index: int = self.__min_index__(index)
+            if self.__queue__[index].weight > self.__queue__[min_index].weight:
+                self.__queue__[index], self.__queue__[min_index] = self.__queue__[min_index], self.__queue__[index]
+                self.__queue__[index].key = index
+                self.__queue__[min_index].key = min_index
+            index = min_index
+
+    def __min_index__(self, index: int) -> int:
+        if index * 2 + 1 > self.__pointer__:
             return index * 2
         else:
-            if self.heap[index * 2].weight <= self.heap[index * 2 + 1].weight:
-                return  index * 2
+            if self.__queue__[index * 2].weight <= self.__queue__[index * 2 + 1].weight:
+                return index * 2
             else:
                 return index * 2 + 1
-    
-    def buildHeap(self, vertices: list):
-        for vertex in vertices:
-            self.insert(vertex)
+
+
+
