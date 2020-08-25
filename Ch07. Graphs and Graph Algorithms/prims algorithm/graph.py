de70da56bbd59a3dd32b2bebecb53e07c5d8503b
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2020-08-24 18:22:14 -0700

    Prim impl - maps

--- Ch07. Graphs and Graph Algorithms/prims algorithm/graph.py
+++ Ch07. Graphs and Graph Algorithms/prims algorithm/graph.py
@@ -1,59 +1,41 @@
-from vertex import Vertex
 from priorityqueue import PriorityQueue
+from vertex import Vertex
 
 class Graph:
     def __init__(self):
-        self.vertices: list = []
-        self.adjacencyList: dict = {}
-        self.prev: dict = {}
+        self.__vertices__: dict = {}
+        self.__prev__: dict = {}
+        self.__adjacency_map__: dict = {}
 
-    def addVertex(self, label: str, weight:int = float("inf")):
-        vertex: Vertex = Vertex(label, weight)
-        vertex.index = len(self.vertices)
-        self.vertices.append(vertex)
-        self.prev[label] = None
-        self.adjacencyList[label] = []
+    def add_vertex(self, label: str):
+        self.__vertices__[label] = Vertex(label)
+        self.__prev__[label] = None
+        self.__adjacency_map__[label]: dict = {}
 
-    def addEdge(self, label1: str, label2: str, weight: int):
-        vertex: Vertex = Vertex(label2, weight)
-        index: int = self.findIndexByLabel(label2)
-        vertex.index = index
-        self.adjacencyList[label1].append(vertex)
-
-        vertex = Vertex(label1, weight)
-        index: int = self.findIndexByLabel(label1)
-        vertex.index = index
-        self.adjacencyList[label2].append(vertex)
+    def add_edge(self, label1: str, label2: str, weight: int = float("inf")):
+        self.__adjacency_map__[label1][label2] = Vertex(label2, weight)
 
     def prims(self, label: str):
-        result: str = ""
-        index: int = self.findIndexByLabel(label)
-        self.vertices[index].weight = 0
-        pq = PriorityQueue()
-        pq.buildHeap(self.vertices)
-        current: Vertex
-        while not pq.isEmpty():
-            current = pq.deleteMin()
-            print(current.label)
-            if self.prev[current.label] is not None:
-                result += self.prev[current.label] + " -> " + current.label + ", "
-            for neighbour in self.adjacencyList[current.label]:
-                if neighbour.weight < self.vertices[neighbour.index].weight:
-                    self.prev[self.vertices[neighbour.index].label] = current.label
-                    self.vertices[neighbour.index].weight = neighbour.weight
-                    pq.decreaseKey(self.vertices[neighbour.index].key)
-        print(result)
-
-    def findIndexByLabel(self, label: str)->int:
-        found: bool = False
-        count: int = 0
-        while count < len(self.vertices) and not found:
-            if self.vertices[count].label == label:
-                found = True
-            else:
-                count += 1
-        if found:
-            return count
-        else:
-            return None
+        self.__vertices__[label].weight = 0
+        pq: PriorityQueue = PriorityQueue()
+        for label in self.__vertices__:
+            pq.insert(self.__vertices__[label])
         
+        result: str = ""
+        while not pq.is_empty():
+            v: Vertex = pq.delete_min()
+            if self.__prev__[v.label] is not None:
+                result += self.__prev__[v.label] + " - " + v.label + ", "
+            for neighbour_label in self.__adjacency_map__[v.label]:
+                neighbour: Vertex = self.__adjacency_map__[v.label][neighbour_label]
+                vertex: Vertex = self.__vertices__[neighbour_label]
+                if neighbour.weight < vertex.weight:
+                    self.__prev__[neighbour_label] = v.label
+                    vertex.weight = neighbour.weight
+                    pq.decrease_key(vertex.key)
+
+        print(result)
+    
+    
+
+    
--- Ch07. Graphs and Graph Algorithms/prims algorithm/main.py
+++ Ch07. Graphs and Graph Algorithms/prims algorithm/main.py
@@ -1,23 +1,23 @@
 from graph import Graph
 
-graph = Graph()
+graph: Graph = Graph()
 
 g = Graph()
-g.addVertex("A")
-g.addVertex("B")
-g.addVertex("C")
-g.addVertex("D")
-g.addVertex("E")
-g.addVertex("F")
+g.add_vertex("A")
+g.add_vertex("B")
+g.add_vertex("C")
+g.add_vertex("D")
+g.add_vertex("E")
+g.add_vertex("F")
 
 
-g.addEdge("A", "B", 4)
-g.addEdge("A", "F", 2)
-g.addEdge("B", "C", 6)
-g.addEdge("F", "B", 3)
-g.addEdge("F", "C", 1)
-g.addEdge("F", "E", 4)
-g.addEdge("C", "D", 3)
-g.addEdge("D", "E", 2)
+g.add_edge("A", "B", 4)
+g.add_edge("A", "F", 2)
+g.add_edge("B", "C", 6)
+g.add_edge("F", "B", 3)
+g.add_edge("F", "C", 1)
+g.add_edge("F", "E", 4)
+g.add_edge("C", "D", 3)
+g.add_edge("D", "E", 2)
 
 g.prims("A")
