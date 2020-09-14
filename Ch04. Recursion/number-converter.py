bd5a79e70543ff7197893df6430b5d82f2c20435
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2020-09-14 18:34:12 +0200

    Remove recursion directory

--- Ch04. Recursion/number-converter.py
+++
@@ -1,9 +0,0 @@
-
-def converter(number: int, base: int)->str:
-    digits = "0123456789ABCDEF"
-    if number < base:
-        return digits[number]
-    else:
-        return converter(number // base, base) + digits[number % base]
-
-print(converter(1453, 16)) # 5AD
