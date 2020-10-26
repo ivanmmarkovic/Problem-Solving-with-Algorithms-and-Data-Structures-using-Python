f246474bad37ced839e8be690eece00e4c511004
Ivan Markovic <ivanmdmarkovic@gmail.com>
2020-10-26 15:47:54 +0100

    Update graph.py

--- Ch07. Graphs and Graph Algorithms/is-graph-bipartite/graph.py
+++ Ch07. Graphs and Graph Algorithms/is-graph-bipartite/graph.py
@@ -2,44 +2,37 @@ from queue import Queue
 
 class Graph:
     def __init__(self):
-        self.__vertices__:dict = {}
-        self.__adjacency_map__: dict = {}
+        self.__vertices__: list = []
+        self.__adjacency_list__: dict = {}
         self.__colors__: dict = {}
-        self.__colors__["red"]: list = []
-        self.__colors__["blue"]: list = []
 
+    def add_vertex(self, label: str):
+        self.__vertices__.append(label)
+        self.__adjacency_list__[label]: list = []
 
-    def add_vertex(self, vertex: str):
-        self.__vertices__[vertex] = vertex
-        self.__adjacency_map__[vertex]:list = []
+    def add_edge(self, label1: str, label2: str):
+        self.__adjacency_list__[label1].append(label2)
+        self.__adjacency_list__[label2].append(label1)
 
-    def add_edge(self, vertex1: str, vertex2: str):
-        self.__adjacency_map__[vertex1].append(vertex2)
-        self.__adjacency_map__[vertex2].append(vertex1)
-
-    def bipartite_check(self, vertex) -> bool:
-        q: Queue = Queue()
+    def is_bipartite(self) -> bool:
         for vertex in self.__vertices__:
-            if vertex in self.__colors__["red"] or vertex in self.__colors__["blue"]:
-                continue
-            self.__colors__["red"].append(vertex)
-            q.enqueue(vertex)
+            if vertex not in self.__colors__:
+                self.__colors__[vertex] = "red"
+                q: Queue = Queue()
+                q.enqueue(vertex)
 
-            while not q.is_empty():
-                tmp: str = q.dequeue()
-                if tmp in self.__colors__["red"]:
-                    current_color = "red"
-                    opposite_color = "blue"
-                else:
-                    current_color = "blue"
-                    opposite_color = "red"
-                for neighbour in self.__adjacency_map__[tmp]:
-                    if neighbour in self.__colors__[current_color]:
-                        return False
-                    if neighbour not in self.__colors__[opposite_color]:
-                        self.__colors__[opposite_color].append(neighbour)
-                        q.enqueue(neighbour)
+                current: str = None
+                while not q.is_empty():
+                    current = q.dequeue()
+                    for neighbour in self.__adjacency_list__[current]:
+                        if neighbour not in self.__colors__:
+                            if self.__colors__[current] == "red":
+                                self.__colors__[neighbour] = "blue"
+                            else:
+                                self.__colors__[neighbour] = "red"
+                            q.enqueue(neighbour)
+                        else:
+                            if self.__colors__[neighbour] == self.__colors__[current]:
+                                return False
         return True
-
-
-    
+                            
