f04aa5f18b2c65465175ad70ae04fc2e9b4d403a
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2020-11-20 23:03:32 +0100

    removed old Dijkstra and Prim

--- Ch07. Graphs and Graph Algorithms/dijkstra/dijkstra priority queue/vertex.py
+++
@@ -1,7 +0,0 @@
-
-class Vertex:
-    def __init__(self, label: str = None, weight: int = float("inf"), key: int = None):
-        self.label: str = label
-        self.weight: int = weight
-        self.key: int = key
-
