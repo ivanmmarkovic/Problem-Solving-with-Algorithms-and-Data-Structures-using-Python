bffe7b30494131e2f3d16712b4b1478ebcb01419
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2020-11-20 15:56:06 +0100

    Removed dijkstra

--- graphs/prim algorithm/graph.py
+++ graphs/prim algorithm/graph.py
@@ -15,22 +15,26 @@ class Graph:
 
     def add_edge(self, label1: str = None, label2: str = None, weight: int = float("inf")):
         self._adjacency_map[label1][label2] = Vertex(label2, weight)
+        self._adjacency_map[label2][label1] = Vertex(label1, weight)
 
-    def dijkstra(self, label: str):
+    def prims(self, label: str):
+        result: str = ""
         self._vertices[label].set_weight(0)
         pq: PriorityQueue = PriorityQueue()
         for vertex_label in self._vertices:
             pq.insert(self._vertices[vertex_label])
         while not pq.is_empty():
             vertex: Vertex = pq.delete_min()
+            if self._prev[vertex.get_label()] is not None:
+                result += self._prev[vertex.get_label()] + " -> " + vertex.get_label() + ", "
             for neighbour_label in self._adjacency_map[vertex.get_label()]:
                 neighbour_from_adjacency_map: Vertex = self._adjacency_map[vertex.get_label()][neighbour_label]
                 neighbour_from_vertices: Vertex = self._vertices[neighbour_label]
-                if neighbour_from_vertices.get_weight() > \
-                        vertex.get_weight() + neighbour_from_adjacency_map.get_weight():
-                    neighbour_from_vertices.set_weight(vertex.get_weight() + neighbour_from_adjacency_map.get_weight())
+                if neighbour_from_vertices.get_weight() > neighbour_from_adjacency_map.get_weight():
+                    neighbour_from_vertices.set_weight(neighbour_from_adjacency_map.get_weight())
                     self._prev[neighbour_from_vertices.get_label()] = vertex.get_label()
                     pq.decrease_key(neighbour_from_vertices.get_key())
+        print(result)
 
     def return_path(self, end_label: str = None) -> str:
         if self._prev[end_label] is None:
