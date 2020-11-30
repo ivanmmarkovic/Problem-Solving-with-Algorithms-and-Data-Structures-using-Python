46072c1e8d77b5947d0c5148e4bb8ee366f598ac
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2020-11-30 14:28:16 +0100

    queue fixed size array implementation

--- queue/queue-array-impl.py
+++ queue/queue-array-impl.py
@@ -9,7 +9,24 @@ class Queue:
         return self._queue.pop()
 
     def size(self) -> int:
-        return len(self.queue)
+        return len(self._queue)
 
     def is_empty(self) -> bool:
         return len(self._queue) == 0
+
+
+def hot_potato(namelist, number):
+    q = Queue()
+
+    for name in namelist:
+        q.enqueue(name)
+
+    while q.size() > 1:
+        for i in range(number):
+            q.enqueue(q.dequeue())
+        q.dequeue()
+
+    return q.dequeue()
+
+
+print(hot_potato(["Bill", "David", "Susan", "Jane", "Kent", "Brad"], 7))
