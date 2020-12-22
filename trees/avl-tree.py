0513902044c84b02b508a659c888ade375578425
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2020-12-22 15:11:41 -0800

    AVL tree

--- trees/avl-tree.py
+++ trees/avl-tree.py
@@ -1,10 +1,14 @@
 class TreeNode:
-    def __init__(self, key=None, value=None, parent=None, left=None, right=None):
+    def __init__(self, key=None, value=None, parent=None, left=None, right=None,
+                 left_subtree_height: int = 0, right_subtree_height: int = 0, balance_factor: int = 0):
         self.key = key
         self.value = value
         self.parent = parent
         self.left = left
         self.right = right
+        self.left_subtree_height: int = left_subtree_height
+        self.right_subtree_height: int = right_subtree_height
+        self.balance_factor : int = balance_factor
 
     def has_left_child(self) -> bool:
         return self.left is not None
@@ -74,12 +78,14 @@ class BinarySearchTree:
             else:
                 root.left = TreeNode(key, value, root)
                 self.elements += 1
+                self._update_balance_factor(root)
         else:
             if root.has_right_child():
                 self._put(root.right, key, value)
             else:
                 root.right = TreeNode(key, value, root)
                 self.elements += 1
+                self._update_balance_factor(root)
 
     def get(self, key) -> TreeNode:
         if self.is_empty():
@@ -128,6 +134,7 @@ class BinarySearchTree:
                 tmp_key = max_node.key
                 tmp_value = max_node.value
                 self.delete(tmp_key)
+                # keep pointer to that node, not root, root might change
                 node_to_delete.key = tmp_key
                 node_to_delete.value = tmp_value
             else:
@@ -138,7 +145,9 @@ class BinarySearchTree:
                 self.root.parent = None
                 self.elements -= 1
         else:
+            parent: TreeNode = None
             if node_to_delete.is_leaf():
+                parent = node_to_delete.parent
                 if node_to_delete.is_left_child():
                     node_to_delete.parent.left = None
                 else:
@@ -152,6 +161,7 @@ class BinarySearchTree:
                 node_to_delete.key = tmp_key
                 node_to_delete.value = tmp_value
             elif node_to_delete.has_left_child():
+                parent = node_to_delete.parent
                 self.elements -= 1
                 if node_to_delete.is_left_child():
                     node_to_delete.parent.left = node_to_delete.left
@@ -159,12 +169,15 @@ class BinarySearchTree:
                     node_to_delete.parent.right = node_to_delete.left
                 node_to_delete.left.parent = node_to_delete.parent
             else:
+                parent = node_to_delete.parent
                 self.elements -= 1
                 if node_to_delete.is_left_child():
                     node_to_delete.parent.left = node_to_delete.right
                 else:
                     node_to_delete.parent.right = node_to_delete.right
                 node_to_delete.right.parent = node_to_delete.parent
+            if parent is not None:
+                self._update_balance_factor(parent)
 
     def find_min(self) -> TreeNode:
         if self.is_empty():
@@ -181,3 +194,71 @@ class BinarySearchTree:
         while node.right is not None:
             node = node.right
         return node
+
+    def _update_balance_factor(self, root: TreeNode):
+        old_balance_factor: int = root.balance_factor
+        if root.has_left_child():
+            root.left_subtree_height = max(root.left.left_subtree_height, root.left.right_subtree_height) + 1
+        else:
+            root.left_subtree_height = 0
+        if root.has_right_child():
+            root.right_subtree_height = max(root.right.left_subtree_height, root.right.right_subtree_height) + 1
+        else:
+            root.right_subtree_height = 0
+        root.balance_factor = root.left_subtree_height - root.right_subtree_height
+        if root.balance_factor < -1 or root.balance_factor > 1:
+            self._rebalance(root)
+            return
+        if root.balance_factor != old_balance_factor and root.has_parent():
+            self._update(root.parent)
+
+    def _rebalance(self, root: TreeNode):
+        if root.balance_factor < 0:
+            if root.right.balance_factor > 0:
+                self._rotate_right(root.right)
+            else:
+                self._rotate_left(root)
+        else:
+            if root.left.balance_factor < 0:
+                self._rotate_left(root.left)
+            else:
+                self._rotate_right(root)
+
+    def _rotate_left(self, root: TreeNode):
+        old_root: TreeNode = root
+        new_root: TreeNode = old_root.right
+        old_root.right = new_root.left
+        if new_root.has_left_child():
+            new_root.left.parent = old_root
+        new_root.parent = old_root.parent
+        if old_root.has_parent():
+            if old_root.is_left_child():
+                old_root.parent.left = new_root
+            else:
+                old_root.parent.right = new_root
+        else:
+            self.root = new_root
+        old_root.parent = new_root
+        new_root.left = old_root
+        self._update_balance_factor(old_root)
+
+    def _rotate_right(self, root: TreeNode):
+        old_root: TreeNode = root
+        new_root: TreeNode = old_root.left
+        old_root.left = new_root.right
+        if new_root.has_right_child():
+            new_root.right.parent = old_root
+        new_root.parent = old_root.parent
+        if old_root.has_parent():
+            if old_root.is_left_child():
+                old_root.parent.left = new_root
+            else:
+                old_root.parent.right = new_root
+        else:
+            self.root = new_root
+        old_root.parent = new_root
+        new_root.right = old_root
+        self._update_balance_factor(old_root)
+
+
+
