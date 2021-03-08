64761a414e761783e807f0d740f0557fac901c2d
Ivan Markovic <ivanmdmarkovic@gmail.com>
2021-03-08 19:02:55 +0100

    Update graph.py

--- graphs/is-graph-bipartite/graph.py
+++ graphs/is-graph-bipartite/graph.py
@@ -3,38 +3,39 @@ from queue import Queue
 
 class Graph:
     def __init__(self):
-        self._vertices: list = []
-        self._colors: dict = {}
-        self._adjacency_matrix: dict = {}
+        self.vertices: list = []
+        self.adjacency_list: dict = {}
+        self.color: dict = {}
 
-    def add_vertex(self, label: str):
-        self._vertices.append(label)
-        self._colors[label] = None
-        self._adjacency_matrix[label]: list = []
+    def add_vertex(self, label: str = None):
+        self.vertices.append(label)
+        self.adjacency_list[label]: list = []
+        self.color[label] = None
 
-    def add_edge(self, label1: str, label2: str):
-        self._adjacency_matrix[label1].append(label2)
-        self._adjacency_matrix[label2].append(label1)
+    def add_edge(self, label1: str = None, label2: str = None):
+        self.adjacency_list[label1].append(label2)
+        self.adjacency_list[label2].append(label1)
 
-    def bipartite_check(self) -> bool:
-        for vertex in self._vertices:
-            if self._colors[vertex] is not None:
+    def is_bipartite(self) -> bool:
+        for vertex in self.vertices:
+            if self.color[vertex] is not None:
                 continue
-            self._colors[vertex] = "red"
             q: Queue = Queue()
+            self.color[vertex] = "red"
             q.enqueue(vertex)
             while not q.is_empty():
-                v = q.dequeue()
-                for neighbour in self._adjacency_matrix[v]:
-                    if self._colors[neighbour] == self._colors[v]:
+                tmp: str = q.dequeue()
+                for neighbour in self.adjacency_list[tmp]:
+                    if self.color[neighbour] == self.color[tmp]:
                         return False
-                    if self._colors[neighbour] is None:
-                        if self._colors[v] == "red":
-                            self._colors[neighbour] = "blue"
+                    if self.color[neighbour] is None:
+                        if self.color[tmp] == "red":
+                            self.color[neighbour] = "blue"
                         else:
-                            self._colors[neighbour] = "red"
+                            self.color[neighbour] = "red"
                         q.enqueue(neighbour)
         return True
+
 
 
 
