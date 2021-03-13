3a4c639910e7a4cbf60a203bcb2a4dc2100c421d
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2021-03-14 00:56:45 +0100

    Kruskals algorithm add edges sorting

--- graphs/minimum-spanning-tree/kruskals-algorithm/graph.py
+++ graphs/minimum-spanning-tree/kruskals-algorithm/graph.py
@@ -5,6 +5,25 @@ def find_root(vertex: Vertex = None) -> Vertex:
         vertex = vertex.root
     return vertex
 
+def sort_edges_by_weight_increasingly(edges: list) -> list:
+    # edges [[label1, label2, weight], [label1, label2, weight]]
+    if len(edges) < 2:
+        return edges
+        
+    pivot_index: int = len(edges) // 2
+    pivot_value = edges[pivot_index][2] 
+    left_list: list = []
+    right_list: list = []
+    for i in range(len(edges)):
+        if i != pivot_index:
+            if edges[i][2] < pivot_value:
+                left_list.append(edges[i])
+            else:
+                right_list.append(edges[i])
+        
+    return sort_edges_by_weight_increasingly(left_list) + [edges[pivot_index]] + sort_edges_by_weight_increasingly(right_list)
+
+
 
 class Graph:
     def __init__(self):
@@ -18,9 +37,13 @@ class Graph:
         if vertex_1_label not in self.vertices or vertex_2_label not in self.vertices:
             raise Exception("Invalid label name")
         self.edges.append([vertex_1_label, vertex_2_label, weight])
+        self.edges.append([vertex_2_label, vertex_1_label, weight])
 
     def kruskal(self):
+        print(self.edges)
         # sort edges by weight increasingly
+        self.edges = sort_edges_by_weight_increasingly(self.edges)
+        print(self.edges)
         minimum_spanning_tree: list = []
         for edge_list in self.edges:
             vertex_one: Vertex = self.vertices[edge_list[0]]
--- graphs/minimum-spanning-tree/kruskals-algorithm/main.py
+++ graphs/minimum-spanning-tree/kruskals-algorithm/main.py
@@ -9,14 +9,14 @@ g.add_vertex('c')
 g.add_vertex('e')
 g.add_vertex('f')
 
-g.add_edge('a', 'f')
-g.add_edge('a', 'b')
+g.add_edge('a', 'f', 4)
+g.add_edge('a', 'b', 23)
 
-g.add_edge('b', 'd')
-g.add_edge('b', 'c')
-g.add_edge('c', 'd')
+g.add_edge('b', 'd', 17)
+g.add_edge('b', 'c', 3)
+g.add_edge('c', 'd', 41)
 
-g.add_edge('c', 'f')
-g.add_edge('f', 'e')
+g.add_edge('c', 'f', 2)
+g.add_edge('f', 'e', 1)
 
 g.kruskal()
