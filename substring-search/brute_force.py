91e24c221ef6b32bd70266fd8f468e94c9763f25
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2021-03-21 01:02:47 +0100

    substring search brute force minor fix

--- substring-search/brute_force.py
+++ substring-search/brute_force.py
@@ -4,9 +4,11 @@
 # m - length of the text
 # n - length of pattern
 def search(text: str, pattern: str) -> int:
-
+    
+    t:int = 0
     last_index: int = len(text) - len(pattern) + 1
-    for t in range(last_index):
+    
+    while t <= len(text) - len(pattern):
         p:int = 0
         while p < len(pattern):
             if text[t + p] != pattern[p]:
@@ -16,6 +18,7 @@ def search(text: str, pattern: str) -> int:
         if p == len(pattern):
             return t
         t += 1
+    
     return -1
 
 
