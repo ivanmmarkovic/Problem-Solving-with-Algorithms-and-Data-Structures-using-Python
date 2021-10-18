1e77cdd80f4d0ee320a3fcfafd4ac0251fa84f2d
Ivan Markovic <ivanmdmarkovic@gmail.com>
2021-10-18 19:07:53 +0200

    Update circular-deque.py

--- deque/circular-deque.py
+++ deque/circular-deque.py
@@ -1,78 +1,85 @@
+
+from typing import Any, List
+
+
 class Deque:
-    def __init__(self, length: int = 10):
-        self._length: int = length
-        self._deque: list = [None] * self._length
-        self._front = self._rear = -1
-        self._count: int = 0
 
-    def add_front(self, item):
-        if self.is_full():
-            raise Exception("Deque is full")
-        if self.is_empty():
-            self._front = self._rear = 0
-        elif self._front == 0:
-            self._front = self._length - 1
-        else:
-            self._front -= 1
-        self._deque[self._front] = item
-        self._count += 1
-
-    def add_rear(self, item):
-        if self.is_full():
-            raise Exception("Deque is full")
-        if self.is_empty():
-            self._front = self._rear = 0
-        else:
-            self._rear = (self._rear + 1) % self._length
-        self._deque[self._rear] = item
-        self._count += 1
-
-    def remove_front(self):
-        if self.is_empty():
-            raise Exception("Deque is empty")
-        ret_value = self._deque[self._front]
-        if self._front == self._rear:
-            self._front = self._rear = -1
-        else:
-            self._front = (self._front + 1) % self._length
-        self._count -= 1
-        return ret_value
-
-    def remove_rear(self):
-        if self.is_empty():
-            raise Exception("Deque is empty")
-        ret_value = self._deque[self._rear]
-        if self._front == self._rear:
-            self._front = self._rear = -1
-        elif self._rear == 0:
-            self._rear = self._length - 1
-        else:
-            self._rear = self._rear - 1
-        self._count -= 1
-        return ret_value
-
-    def get_front(self):
-        if self.is_empty():
-            raise Exception("Deque is empty")
-        return self._deque[self._front]
-
-    def get_rear(self):
-        if self.is_empty():
-            raise Exception("Deque is empty")
-        return self._deque[self._rear]
-
-    def is_full(self) -> bool:
-        return (self._rear + 1) % self._length == self._front
+    def __init__(self, capacity: int = 10) -> None:
+        self.capacity: int = capacity
+        self.length: int = 0
+        self._deque: List[Any] = [None] * self.capacity
+        self.front: int = -1
+        self.rear: int = -1
 
     def is_empty(self) -> bool:
-        return self._front == -1
+        return self.front == -1
+
+    def is_full(self) -> bool:
+        return (self.rear + 1) % self.capacity == self.front
 
     def size(self) -> int:
-        return self._count
+        return self.length
+
+    def add_front(self, item: Any):
+        if self.is_full():
+            raise Exception('Deque is full')
+        if self.is_empty(): # self.rear == -1
+            self.front = self.rear = 0
+        elif self.front == 0:
+            self.front = self.capacity - 1
+        else:
+            self.front -= 1
+        self._deque[self.front] = item
+        self.length += 1
+
+    def add_rear(self, item: Any):
+        if self.is_full():
+            raise Exception('Deque is full')
+        if self.is_empty(): # self.rear == -1
+            self.front = self.rear = 0
+        else:
+            self.rear = (self.rear + 1) % self.capacity
+        self._deque[self.rear] = item
+        self.length += 1
+
+    def remove_front(self) -> Any:
+        if self.is_empty():
+            raise Exception('Deque is empty')
+        item: Any = self._deque[self.front]
+        if self.front == self.rear:
+            self.front = self.rear = -1
+        else:
+            self.front = (self.front + 1) % self.capacity
+        self.length -= 1
+        return item
+
+    def remove_rear(self) -> Any:
+        if self.is_empty():
+            raise Exception('Deque is empty')
+        item: Any = self._deque[self.rear]
+        if self.rear == self.front:
+            self.rear = self.front = -1
+        elif self.rear == 0:
+            self.rear = self.capacity - 1
+        else:
+            self.rear -= 1
+        self.length -= 1
+        return item
+
+    def get_front(self) -> Any:
+        if self.is_empty():
+            raise Exception('Deque is empty')
+        return self._deque[self.front]
+
+    def get_rear(self) -> Any:
+        if self.is_empty():
+            raise Exception('Deque is empty')
+        return self._deque[self.rear]
 
 
-def is_palindrome(string: str) -> bool:
-    d: Deque = Deque(len(string))
+def is_palindrome(string:str='') -> bool:
+    d: Deque = Deque()
+
     for character in string:
         d.add_rear(character)
 
@@ -80,10 +87,10 @@ def is_palindrome(string: str) -> bool:
     while d.size() > 1 and palindrome:
         if d.remove_front() != d.remove_rear():
             palindrome = False
+    
     return palindrome
 
-
-print(is_palindrome("radar"))
-print(is_palindrome("radr"))
-
-
+print(is_palindrome('radar'))
+print(is_palindrome('radr'))
+print(is_palindrome('r'))
+print(is_palindrome(''))
