aa9e534c88465ec2b36217736105a7e79c7cbcfa
Ivan Markovic <ivanmdmarkovic@gmail.com>
2021-10-18 18:46:05 +0200

    Update deque.py

--- deque/deque.py
+++ deque/deque.py
@@ -1,22 +1,11 @@
+
+from typing import Any, List
+
+
 class Deque:
-    def __init__(self):
-        self._deque: list = []
 
-    def add_front(self, item):
-        self._deque.insert(0, item)
-
-    def add_rear(self, item):
-        self._deque.append(item)
-
-    def remove_front(self):
-        if self.is_empty():
-            raise Exception("Deque is empty")
-        return self._deque.pop(0)
-
-    def remove_rear(self):
-        if self.is_empty():
-            raise Exception("Deque is empty")
-        return self._deque.pop()
+    def __init__(self) -> None:
+        self._deque: List[Any] = []
 
     def is_empty(self) -> bool:
         return len(self._deque) == 0
@@ -24,9 +13,27 @@ class Deque:
     def size(self) -> int:
         return len(self._deque)
 
+    def add_front(self, item: Any):
+        self._deque.insert(0, item)
 
-def is_palindrome(string: str) -> bool:
+    def add_rear(self, item: Any):
+        self._deque.append(item)
+
+    def remove_front(self) -> Any:
+        if self.is_empty():
+            raise Exception('Deque is empty')
+        return self._deque.pop(0)
+
+    def remove_rear(self) -> Any:
+        if self.is_empty():
+            raise Exception('Deque is empty')
+        return self._deque.pop()
+
+
+
+def is_palindrome(string:str='') -> bool:
     d: Deque = Deque()
+
     for character in string:
         d.add_rear(character)
 
@@ -34,10 +41,10 @@ def is_palindrome(string: str) -> bool:
     while d.size() > 1 and palindrome:
         if d.remove_front() != d.remove_rear():
             palindrome = False
+    
     return palindrome
 
-
-print(is_palindrome("radar"))
-print(is_palindrome("radr"))
-
-
+print(is_palindrome('radar'))
+print(is_palindrome('radr'))
+print(is_palindrome('r'))
+print(is_palindrome(''))
