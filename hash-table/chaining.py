ad9fc70388b8930cbcd4bb2a24c7fc20cfea551e
Ivan Markovic <ivanmdmarkovic@gmail.com>
2021-10-22 17:38:05 +0200

    Update chaining.py

--- hash-table/chaining.py
+++ hash-table/chaining.py
@@ -1,112 +1,133 @@
 
+
+from typing import Any, List
+
 class KeyValue:
-    def __init__(self, key: int = None, value: str = None):
+
+    def __init__(self, key: int, value: Any) -> None:
         self.key: int = key
-        self.value: str = value
+        self.value: Any = value
 
-class HashMap:
-    def __init__(self, length: int = 11):
-        self._length: int = length
-        self._items: list = [None] * self._length
-        self._elements_count: int = 0
 
-    def put(self, key: int, value: str):
-        hash: int = self.hash(key)
-        if self._items[hash] is None:
-            self._items[hash] = [KeyValue(key, value)]
-            self._elements_count += 1
+class HashTable:
+
+
+    def __init__(self, capacity:int = 11) -> None:
+        self.capacity: int = capacity
+        self.length: int = 0
+        self.table: List[List[KeyValue]] = [None] * self.capacity
+
+    
+    def put(self, key: int, value: Any) -> int:
+        index: int = self.hash(key)
+        if self.table[index] is None:
+            self.table[index] = [KeyValue(key, value)]
+            self.length += 1
         else:
-            i: int = 0
             found: bool = False
-            while i < len(self._items[hash]) and not found:
-                if self._items[hash][i].key == key:
+            i: int = 0
+            items: List[KeyValue] = self.table[index]
+            while i < len(items) and not found:
+                if items[i].key == key:
                     found = True
                 else:
                     i += 1
             if found:
-                self._items[hash][i].value = value
+                items[i].value = value
             else:
-                self._items[hash].append(KeyValue(key, value))
-                self._elements_count += 1
+                items.append(KeyValue(key, value))
+                self.length += 1
 
-    def get(self, key: int) -> str:
-        hash: int = self.hash(key)
-        if self._items[hash] is None:
+
+    def get(self, key: int) -> Any:
+        index: int = self.hash(key)
+        if self.table[index] is None:
             return None
         else:
-            i: int = 0
             found: bool = False
-            while i < len(self._items[hash]) and not found:
-                if self._items[hash][i].key == key:
+            i: int = 0
+            items: List[KeyValue] = self.table[index]
+            while i < len(items) and not found:
+                if items[i].key == key:
                     found = True
                 else:
                     i += 1
             if found:
-                return self._items[hash][i].value
+                return items[i].value
             else:
                 return None
 
+
     def contains(self, key: int) -> bool:
-        hash: int = self.hash(key)
-        if self._items[hash] is None:
+        index: int = self.hash(key)
+        if self.table[index] is None:
             return False
         else:
-            i: int = 0
             found: bool = False
-            while i < len(self._items[hash]) and not found:
-                if self._items[hash][i].key == key:
-                    found = True
-                else:
-                    i += 1
-            return found
-
-    def delete(self, key):
-        hash: int = self.hash(key)
-        if self._items[hash] is None:
-            return
-        else:
             i: int = 0
-            found: bool = False
-            while i < len(self._items[hash]) and not found:
-                if self._items[hash][i].key == key:
+            items: List[KeyValue] = self.table[index]
+            while i < len(items) and not found:
+                if items[i].key == key:
                     found = True
                 else:
                     i += 1
             if found:
-                self._items[hash].pop(i)
-                self._elements_count -= 1
+                return True
+            else:
+                return False
 
-    def size(self) -> int:
-        return self._elements_count
+
+    def delete(self, key: int) -> None:
+        index: int = self.hash(key)
+        if self.table[index] is None:
+            return None
+        else:
+            found: bool = False
+            i: int = 0
+            items: List[KeyValue] = self.table[index]
+            while i < len(items) and not found:
+                if items[i].key == key:
+                    found = True
+                else:
+                    i += 1
+            if found:
+                items.pop(i)
+            return None
+
 
     def hash(self, key: int) -> int:
-        return key % self._length
+        return key % self.capacity
 
 
-hm: HashMap = HashMap()
-hm.put(11, "string 11")
-hm.put(22, "string 22")
-hm.put(33, "string 33")
-hm.put(44, "string 44")
-hm.put(12, "string 12")
-hm.put(21, "string 21")
+    def size(self) -> int:
+        return self.length
 
 
-print("Get 11", hm.get(11))
-print("Get 33", hm.get(33))
-print("Get 21", hm.get(21))
-print("Get 7", hm.get(7))
 
-print("Contains key 7", hm.contains(7))
-print("Contains key 33", hm.contains(33))
+ht: HashTable = HashTable()
+ht.put(11, 'string 11')
+ht.put(22, 'string 22')
+ht.put(33, 'string 33')
+ht.put(44, 'string 44')
 
+ht.put(21, 'string 21')
+ht.put(12, 'string 12')
 
-print("Delete key 7", hm.delete(7))
-print("Delete key 33", hm.delete(33))
+print(ht.size())
+print('Get 11', ht.get(11))
+print('Get 33', ht.get(33))
+print('Get 147', ht.get(147))
+print('----------------------------------------')
 
-print("Contains key 33", hm.contains(33))
+print('Contains 22', ht.contains(22))
+ht.delete(22)
+print(ht.size())
+print('Contains 22', ht.contains(22))
+print('----------------------------------------')
 
+print('Contains 77', ht.contains(77))
+ht.put(44, 'string 144')
+ht.put(77, 'string 77')
 
-hm.put(14, "string 14")
-
-print("Contains key 33", hm.contains(33))
+print(ht.size())
+print('Contains 77', ht.contains(77))
