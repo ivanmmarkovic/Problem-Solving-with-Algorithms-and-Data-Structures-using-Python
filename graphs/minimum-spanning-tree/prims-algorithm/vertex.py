3dfac51f71a500c78bcde0258cced8287e33e88d
Ivan Markovic <ivanmdmarkovic@gmail.com>
2022-04-08 14:31:28 +0200

    Update vertex.py

--- graphs/minimum-spanning-tree/prims-algorithm/vertex.py
+++ graphs/minimum-spanning-tree/prims-algorithm/vertex.py
@@ -1,5 +1,7 @@
 class Vertex:
-    def __init__(self, label: str = None, weight: int = float("inf"), key: int = None):
-        self.label: str = label
-        self.weight: int = weight
-        self.key: int = key
+
+    def __init__(self, label:str=None, weight:int=float('inf'), index:int=None) -> None:
+        self.label:str = label
+        self.weight:int = weight
+        self.index:int = index
+
