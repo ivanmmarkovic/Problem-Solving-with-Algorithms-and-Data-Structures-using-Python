c4d2e2ec68460d98076474a9ae698bc815c7be84
Ivan Markovic <ivanmdmarkovic@gmail.com>
2022-07-13 17:08:58 +0200

    Update stack_two_queues.py

--- stack/stack_two_queues.py
+++ stack/stack_two_queues.py
@@ -63,10 +63,3 @@ class MyStack:
 
         
 
-
-# Your MyStack object will be instantiated and called as such:
-# obj = MyStack()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.empty()
